fde19a2676e5ac181fc3d19de215041d5954ce0d
AustinsGroove <vadimgajiy@gmail.com>
2023-10-23 21:12:51 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,49 +1,123 @@
+import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+const axios = require('axios').default;
 
 const elements = {
   form: document.querySelector('form.search-form'),
   gallery: document.querySelector('ul.gallery'),
+  loadMoreBtn: document.querySelector('button.load-more'),
+  endNotify: document.querySelector('p.end-notify'),
 };
 
+let serchParameter;
+let lightbox;
+let totalHits;
+let page = 1;
+let perPage = 40;
+
 elements.form.addEventListener('submit', searchHandler);
+elements.loadMoreBtn.addEventListener('click', loadMoreFn);
+
+Notiflix.Notify.init({
+  width: '300px',
+  position: 'right-top',
+  distance: '20px',
+  borderRadius: '10px',
+  opacity: 1,
+  timeout: 5000,
+});
 
 async function searchHandler(event) {
   event.preventDefault();
-  const serchParameter = event.currentTarget.elements.searchQuery.value;
+  page = 1;
+  if (!elements.loadMoreBtn.classList.contains('hidden')) {
+    elements.loadMoreBtn.classList.add('hidden');
+  }
+  if (!elements.endNotify.classList.contains('hidden')) {
+    elements.endNotify.classList.add('hidden');
+  }
+  elements.gallery.innerHTML = '';
+  serchParameter = event.currentTarget.elements.searchQuery.value;
   try {
     const response = await imageSearchService(serchParameter);
     const itemsArray = response.hits;
-    console.log(itemsArray);
+    totalHits = response.totalHits;
+    if (totalHits) {
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
     if (!itemsArray.length) {
       console.log(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
     renderGallery(itemsArray);
+    lightbox = new SimpleLightbox('ul.gallery .gallery-link');
+    loadMoreBtnAdd();
   } catch (error) {
     console.log(error);
+    Notiflix.Notify.failure(`${error}`);
+  }
+}
+
+async function loadMoreFn() {
+  elements.loadMoreBtn.classList.add('hidden');
+  page += 1;
+  try {
+    const response = await imageSearchService(serchParameter);
+    const itemsArray = response.hits;
+    totalHits = response.totalHits;
+    renderGallery(itemsArray);
+    lightbox.refresh();
+    loadMoreBtnAdd();
+    smoothScrollGallery();
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure(`${error}`);
   }
 }
 
 function imageSearchService(serchParameter) {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '40203420-04db41a8a9312c45ba95b8564';
-  const searchParams = new URLSearchParams({
+  const searchParams = {
     key: API_KEY,
     q: serchParameter,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  });
-  return fetch(`${BASE_URL}?${searchParams}`).then(response => {
-    console.log(response);
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+    per_page: perPage,
+    page,
+  };
+  return axios
+    .get(BASE_URL, {
+      params: searchParams,
+    })
+    .then(response => {
+      return response.data;
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
 }
 
 function renderGallery(itemsArray) {
@@ -74,6 +148,23 @@ function renderGallery(itemsArray) {
       `
     )
     .join('');
-  elements.gallery.innerHTML = markup;
-  const lightbox = new SimpleLightbox('ul.gallery .gallery-link');
+  elements.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function loadMoreBtnAdd() {
+  const totalPages = Math.ceil(totalHits / perPage);
+  if (totalPages > page) {
+    elements.loadMoreBtn.classList.remove('hidden');
+  } else {
+    elements.endNotify.classList.remove('hidden');
+  }
+}
+
+function smoothScrollGallery() {
+  const { height } = elements.gallery.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: height * 2,
+    behavior: 'smooth',
+  });
 }
